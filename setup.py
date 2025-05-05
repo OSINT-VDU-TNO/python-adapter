e9c38a65bd82e65a22c1efce249605d2a4075cda
Erik Vullings <erik.vullings@gmail.com>
2025-05-05 17:18:03 +0200

    v2.5.3 Added missing deps from confluent_kafka, attrs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="osint-python-test-bed-adapter",
-    version="2.5.2",
+    version="2.5.3",
     author="TimovdK",
     author_email="timo_kuil@hotmail.com",
     description="Python adapter for Kafka",
@@ -19,7 +19,8 @@ setuptools.setup(
         'requests',
         # dependencies required by confluent_kafka's schema registry
         'authlib',
-        'cachetools'
+        'cachetools',
+        'attrs'
     ],
     packages=setuptools.find_packages(),
     classifiers=[
